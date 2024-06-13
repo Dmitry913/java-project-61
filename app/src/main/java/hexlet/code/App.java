824f9c43436a08d9d1158ccc4d7accfe226d2055
Dmitry913 <dmitry.karp10@gmail.com>
2024-06-14 00:20:06 +0300

    add progression game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.CalculatorGame;
 import hexlet.code.games.EvenGame;
 import hexlet.code.games.GCDGame;
+import hexlet.code.games.ProgressionGame;
 
 import static hexlet.code.Cli.SCANNER;
 import static hexlet.code.Cli.userGreeting;
@@ -13,12 +14,16 @@ public class App {
     public static final int SECOND_GAME = 2;
     public static final int THIRD_GAME = 3;
     public static final int FOURTH_GAME = 4;
+    public static final int FIFTH_GAME = 5;
 
     public static void main(String[] args) {
         printGameMenu();
         int userChoice = Integer.parseInt(SCANNER.nextLine());
         System.out.println();
         switch (userChoice) {
+            case FIFTH_GAME:
+                Engine.runFlow(new ProgressionGame());
+                break;
             case FOURTH_GAME:
                 Engine.runFlow(new GCDGame());
                 break;
@@ -41,6 +46,7 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
     }
---
+++ app/src/main/java/hexlet/code/games/ProgressionGame.java
@@ -0,0 +1,50 @@
+package hexlet.code.games;
+
+public class ProgressionGame implements Game {
+
+    private static final int PROGRESSION_LENGTH_MIN = 5;
+    private static final int PROGRESSION_LENGTH_MAX = 15; // not included
+    private static final int INCREMENT_MIN = 1;
+    private static final int INCREMENT_MAX = 9; // not included
+    private static final int START_VALUE_MIN = 3;
+    private static final int START_VALUE_MAX = 12; // not included
+
+    private int answer;
+
+    @Override
+    public String generateQuestion() {
+        int length = RANDOM_GENERATOR.nextInt(PROGRESSION_LENGTH_MIN, PROGRESSION_LENGTH_MAX);
+        int hidePosition = RANDOM_GENERATOR.nextInt(0, length);
+        int increment = RANDOM_GENERATOR.nextInt(INCREMENT_MIN, INCREMENT_MAX);
+        int startValue = RANDOM_GENERATOR.nextInt(START_VALUE_MIN, START_VALUE_MAX);
+        int previousValue = startValue;
+        StringBuilder questionBuilder = new StringBuilder();
+        if (hidePosition == 0) {
+            questionBuilder.append("..");
+            answer = startValue;
+        } else {
+            questionBuilder.append(startValue);
+        }
+        for (int i = 1; i < length; i++) {
+            int currentValue = previousValue + increment;
+            if (i == hidePosition) {
+                questionBuilder.append(" ..");
+                answer = currentValue;
+            } else {
+                questionBuilder.append(" ").append(currentValue);
+            }
+            previousValue = currentValue;
+        }
+        return questionBuilder.toString().trim();
+    }
+
+    @Override
+    public String getAnswer() {
+        return String.valueOf(answer);
+    }
+
+    @Override
+    public String explainRules() {
+        return "What number is missing in the progression?\n";
+    }
+}
